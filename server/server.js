84d0d0ff9a6b60d668764804b18b2cc8fd93a30b
Rediet Ayalew <rediet.ayalew@drake.edu>
2025-12-02 14:46:25 -0600

    Added changes for deployment

--- server/server.js
+++ server/server.js
@@ -8,7 +8,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_ORIGIN,
+}));
 app.use(express.json());
 
 // MongoDB Connection
